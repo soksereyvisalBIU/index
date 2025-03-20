59969278382aced88f585614b231bece3fb10109
sok serey visal <121630407+soksereyvisalBIU@users.noreply.github.com>
2025-03-21 01:15:43 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Projects from "./pages/projects";
 import Articles from "./pages/articles";
 import ReadArticle from "./pages/readArticle";
 import Contact from "./pages/contact";
-import Notfound from "./pages/404";
+// import Notfound from "./pages/404";
 
 import { TRACKING_ID } from "./data/tracking";
 import "./app.css";
